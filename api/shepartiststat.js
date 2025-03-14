0133316edcc9c4e66132a39278635d8bd6734c61
Tori <74414720+toricodesthings@users.noreply.github.com>
2025-03-14 01:43:10 -0400

    test

--- api/shepartiststat.js
+++ api/shepartiststat.js
@@ -62,7 +62,7 @@ export default async function handler(req, res) {
         
             return res.status(200).json({ message: 'Data inserted successfully' });
     } catch (err) {
-    console.error('Unhandled error:', err);
-    return res.status(500).json({ message: 'Unhandled error', error: err });
+        console.error('Unhandled error occured:', err);
+        return res.status(500).json({ message: 'Unhandled error', error: err });
     }
 };

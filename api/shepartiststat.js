a3617b7bab988fe246eaafc1380560d65f6872cd
Tori <74414720+toricodesthings@users.noreply.github.com>
2025-03-14 14:47:42 -0400

    Slight typo correction

--- api/shepartiststat.js
+++ api/shepartiststat.js
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 // Initialize Supabase client
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);

4997d00a22b80c491f106ba0d6a9eca56835868a
Tori <74414720+toricodesthings@users.noreply.github.com>
2025-03-15 16:02:29 -0400

    Add speed insights for vercel integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home';
 import MusicPage from './Pages/Music'; // renamed import to reflect its use as MusicPage
@@ -14,6 +15,7 @@ const App = () => {
     <Router>
       <div>
         <Analytics />
+        <SpeedInsights />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />

277723aaf64b2da543dc5e386bcee0b25691f280
Tori <74414720+toricodesthings@users.noreply.github.com>
2025-03-26 15:44:30 -0400

    Added image optimization

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,39 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          // Check if the module comes from node_modules
           if (id.includes('node_modules')) {
-            // Extract the package name from the file path
             return id.toString().split('node_modules/')[1].split('/')[0];
           }
         }
       }
     }
   },
-  plugins: [react()],
+  plugins: [react(),
+  ViteImageOptimizer({
+    png: { quality: 92 },
+    jpeg: { quality: 90 },
+    webp: { quality: 92 },
+    avif: { quality: 80 },
+    svg: {
+      plugins: [
+        { name: 'removeViewBox', active: false },
+        { name: 'sortAttrs' },
+      ],
+    },
+  }),
+  ],
   server: {
     port: 5176,
     proxy: {
       '/api': {
         target: 'http://localhost:5180',
         changeOrigin: true,
-        // If your Express routes don't include the /api prefix,
-        // you can uncomment the following line to remove it:
-        // rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }

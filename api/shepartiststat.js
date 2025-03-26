6b8655dc515c0a347ced6c44a7889993ec65c047
Tori <74414720+toricodesthings@users.noreply.github.com>
2025-03-26 03:03:57 -0400

    Use header check instead

--- api/shepartiststat.js
+++ api/shepartiststat.js
@@ -43,12 +43,11 @@ async function getMonthlyListeners() {
 }
 
 export default async function handler(req, res) {
-  const secret = req.headers['x-cron-secret'];
+  const userAgent = req.headers['user-agent'] || '';
 
-  if (secret !== process.env.CRON_SECRET) {
-    return res.status(401).json({ message: 'Nuh uh! Unauthorized' });
+  if (!userAgent.includes('vercel-cron/1.0')) {
+    return res.status(401).json({ error: 'Unauthorized' });
   }
-
   try {
     const accessToken = await getSpotifyAccessToken();
     const artistData = await getArtistData(accessToken);

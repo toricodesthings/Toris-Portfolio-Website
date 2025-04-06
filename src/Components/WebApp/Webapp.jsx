dc544a4b32f4f647438357140364d921841b6e9e
Tori <74414720+toricodesthings@users.noreply.github.com>
2025-04-06 01:46:16 -0400

    oops! wrong link

--- src/Components/WebApp/Webapp.jsx
+++ src/Components/WebApp/Webapp.jsx
@@ -3,84 +3,84 @@ import "./Webapp.css";
 
 
 const Webapp = () => {
-  return (
+return (
     <div className="webapp-browser">
-        <div className="webapp-title">
-            <h1 className="pop-in">My WebApps</h1>
-        </div>
-        <section className="webapp-section">
-            <div className="webapp-grid">
-                <div className="webapp-card">
-                    <a href="convertifile.toridoesthings.xyz" className="card-link">
-                        <h3>Convertifile</h3>
-                        <p>All-in-one File Suite</p>
-                        <p style={{color: "lightgray", fontSize: "0.8rem"}}>(Under Construction!)</p>
-                    </a>
-                </div>
-                <div className="webapp-card">
-                    <a href="#" className="card-link">
-                        <h3>DailistAI</h3>
-                        <p>AI meets Daily Planner</p>
-                    </a>
-                </div>
-                <div className="webapp-card">
-                    <a href="#" className="card-link">
-                        <h3>AImage Toolbox</h3>
-                        <p>Image Upscale & Processing</p>
-                    </a>
-                </div>
-                <div className="webapp-card">
-                    <a href="#" className="card-link">
-                        <h3>TranscrAIb</h3>
-                        <p>S-T-T AI transcriber</p>
-                    </a>
-                </div>
-                <div className="webapp-card">
-                    <a href="#" className="card-link">
-                        <h3>Loudernot</h3>
-                        <p>Audio Loudness Penalty</p>
-                    </a>
-                </div>
-                <div className="webapp-card">
-                    <a href="#" className="card-link">
-                        <h3>Medbase AI</h3>
-                        <p>Medicine Databse</p>
-                    </a>
-                </div>
-                <div className="webapp-card">
-                    <a href="#" className="card-link">
-                        <h3>Cryptolyzer</h3>
-                        <p>Cryptocurrency Analyzer</p>
-                    </a>
-                </div>
-                <div className="webapp-card">
-                    <a href="#" className="card-link">
-                        <h3>SummarAIze</h3>
-                        <p>Source aggregate summary generator</p>
-                    </a>
-                </div>
-                <div className="webapp-card">
-                    <a href="#" className="card-link">
-                         <h3>TypeTest</h3>
-                        <p>Just a Typing Test</p>
-                    </a>
-                </div>
-                <div className="webapp-card">
-                    <a href="#" className="card-link">
-                         <h3>IDLE Game</h3>
-                        <p>...</p>
-                    </a>
-                </div>
-                <div className="webapp-card">
-                    <a href="#" className="card-link">
-                         <h3>Chatily</h3>
-                        <p>A realtime ChatAPP</p>
-                    </a>
-                </div>
+            <div className="webapp-title">
+                    <h1 className="pop-in">My WebApps</h1>
             </div>
-        </section>
+            <section className="webapp-section">
+                    <div className="webapp-grid">
+                            <div className="webapp-card">
+                                    <a href="https://convertifile.toridoesthings.xyz" className="card-link" target="_blank" rel="noopener noreferrer">
+                                            <h3>Convertifile</h3>
+                                            <p>All-in-one File Suite</p>
+                                            <p style={{color: "lightgray", fontSize: "0.8rem"}}>(Under Construction!)</p>
+                                    </a>
+                            </div>
+                            <div className="webapp-card">
+                                    <a href="https://dailistai.example.com" className="card-link" target="_blank" rel="noopener noreferrer">
+                                            <h3>DailistAI</h3>
+                                            <p>AI meets Daily Planner</p>
+                                    </a>
+                            </div>
+                            <div className="webapp-card">
+                                    <a href="https://aimagetoolbox.example.com" className="card-link" target="_blank" rel="noopener noreferrer">
+                                            <h3>AImage Toolbox</h3>
+                                            <p>Image Upscale & Processing</p>
+                                    </a>
+                            </div>
+                            <div className="webapp-card">
+                                    <a href="https://transcraib.example.com" className="card-link" target="_blank" rel="noopener noreferrer">
+                                            <h3>TranscrAIb</h3>
+                                            <p>S-T-T AI transcriber</p>
+                                    </a>
+                            </div>
+                            <div className="webapp-card">
+                                    <a href="https://loudernot.example.com" className="card-link" target="_blank" rel="noopener noreferrer">
+                                            <h3>Loudernot</h3>
+                                            <p>Audio Loudness Penalty</p>
+                                    </a>
+                            </div>
+                            <div className="webapp-card">
+                                    <a href="https://medbaseai.example.com" className="card-link" target="_blank" rel="noopener noreferrer">
+                                            <h3>Medbase AI</h3>
+                                            <p>Medicine Databse</p>
+                                    </a>
+                            </div>
+                            <div className="webapp-card">
+                                    <a href="https://cryptolyzer.example.com" className="card-link" target="_blank" rel="noopener noreferrer">
+                                            <h3>Cryptolyzer</h3>
+                                            <p>Cryptocurrency Analyzer</p>
+                                    </a>
+                            </div>
+                            <div className="webapp-card">
+                                    <a href="https://summaraize.example.com" className="card-link" target="_blank" rel="noopener noreferrer">
+                                            <h3>SummarAIze</h3>
+                                            <p>Source aggregate summary generator</p>
+                                    </a>
+                            </div>
+                            <div className="webapp-card">
+                                    <a href="https://typetest.example.com" className="card-link" target="_blank" rel="noopener noreferrer">
+                                             <h3>TypeTest</h3>
+                                            <p>Just a Typing Test</p>
+                                    </a>
+                            </div>
+                            <div className="webapp-card">
+                                    <a href="https://idlegame.example.com" className="card-link" target="_blank" rel="noopener noreferrer">
+                                             <h3>IDLE Game</h3>
+                                            <p>...</p>
+                                    </a>
+                            </div>
+                            <div className="webapp-card">
+                                    <a href="https://chatily.example.com" className="card-link" target="_blank" rel="noopener noreferrer">
+                                             <h3>Chatily</h3>
+                                            <p>A realtime ChatAPP</p>
+                                    </a>
+                            </div>
+                    </div>
+            </section>
     </div>
-  );
+);
 }
 
 export default Webapp;
